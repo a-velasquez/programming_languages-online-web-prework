cf3bc5814deb217e62cf5f6039be81a91000ecff
Alex Velasquez <jtinnesco@gmail.com>
2019-05-11 00:04:18 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,44 @@
+require 'pry'
+
+
+languages = {
+  :oo => {        #style key is :oo and value is 
+    :ruby => {            #languages key is ruby and value is :type 
+      :type => "interpreted"       #type key is type and value is "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+ 
+  }
+}
+
+#should be {:language => {:type =>"type", :style => [:style]}, 
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |style, data|
+    
+    binding.pry 
+  end
 end
